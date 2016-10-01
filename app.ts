1a35f21fc69653fedf98ed6976505a221ef4b89f
Michael Perez <perez.michael.d@gmail.com>
2016-09-30 20:45:36 -0700

    Set up basic express server

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
 ///<reference path="./typings/index.d.ts"/>
-
-import * as config from 'config';
-import * as express from 'express';
+import * as path       from 'path';
+import * as config     from 'config';
+import * as express    from 'express';
 import * as bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 
 
-let app: express.Express = express();
-const port: number = process.env || 3000;
+const app: express.Express = express();
+const port: number         = process.env || 3000;
 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const server  = app.listen(port, () => {
+    const address = server.address().address;
+    const port = server.address().port;
+    console.log(`Application listening at http://${address}:${port}`);
+});
