94bad617c38983776a22e01637889b45972ee8cd
Michael Perez <perez.michael.d@gmail.com>
2016-10-02 18:34:39 -0700

    Temporarily remove typescript from app.js (renamed from app.ts) until using express.js with typescript is properly understood -- just to get server running

---
+++ app.js
@@ -0,0 +1,25 @@
+// ///<reference path="./typings/globals/node/index.d.ts"/>
+
+// import * as path       from 'path';
+// import * as config     from 'config';
+// import * as express    from 'express';
+// import * as bodyParser from 'body-parser';
+
+// const app: express.Express = express();
+// const port: number         = process.env || 3000;
+
+import path       from 'path';
+import config     from 'config';
+import express    from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = config.get('server.port');
+
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '..')));
+
+const server  = app.listen(port, () => {
+    const { address, port } = server.address();
+    console.log(`Application listening at http://${address}:${port}`);
+});
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript';
 
 export default {
-  entry: 'app.ts',
+  entry: 'app.js',
   dest: 'dist/app.js',
   format: 'cjs',
   plugins: [ typescript() ]
