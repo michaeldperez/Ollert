46dfdfdd99b3caa9f91c4be37aeacee4fd1f8e40
Michael Perez <perez.michael.d@gmail.com>
2016-10-05 22:04:34 -0700

    Add enzyme test framework and write spec for Hello component

---
+++ __tests__/hello.test.tsx
@@ -0,0 +1,13 @@
+///<reference path="../typings/index.d.ts"/>
+
+import * as React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { Hello } from '../src/components/Hello';
+
+describe("Hello Component", function() {
+  it("Renders the correct compiler and framework", function() {
+    const wrapper = shallow(<Hello compiler="TypeScript" framework="React" />);
+    expect(wrapper.prop('compiler')).toEqual('TypeScript');
+    expect(wrapper.prop('framework')).toEqual('React');
+  });
+});
