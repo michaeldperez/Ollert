5e34077e985fd2c2dc4318faf14cf191789d4585
Michael Perez <perez.michael.d@gmail.com>
2016-10-06 17:36:45 -0700

    Fix test suite to correctly use enzyme test framework

--- __tests__/hello.test.tsx
+++ __tests__/hello.test.tsx
@@ -6,7 +6,7 @@ import { Hello } from '../src/components/Hello';
 
 describe("Hello Component", function() {
   it("Renders the correct compiler and framework", function() {
-    const wrapper = shallow(<Hello compiler="TypeScript" framework="React" />);
+    const wrapper = mount(<Hello compiler="TypeScript" framework="React" />);
     expect(wrapper.prop('compiler')).toEqual('TypeScript');
     expect(wrapper.prop('framework')).toEqual('React');
   });
