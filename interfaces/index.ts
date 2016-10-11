6c9664e6a0e905bb3f9ca887a40ff06d04ca198e
Michael Perez <perez.michael.d@gmail.com>
2016-10-10 20:07:27 -0700

    Add host and database connection interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,28 +1,40 @@
 
 export interface ICard {
-    text: string,
+    text:      string,
     createdAt: Date,
     updatedAt: Date
 }
 
 export interface IList {
-    name: string,
-    cards: Array<ICard>,
+    name:      string,
+    cards:     Array<ICard>,
     createdAt: Date,
     updatedAt: Date
 }
 
 export interface IBoard {
-    name: string,
-    lists: Array<IList>,
+    name:      string,
+    lists:     Array<IList>,
     createdAt: Date,
     updatedAt: Date
 }
 
 export interface IUser {
-    username: string,
-    pasword: string,
-    boards: Array<IBoard>,
+    username:  string,
+    pasword:   string,
+    boards:    Array<IBoard>,
     createdAt: Date,
     updatedAt: Date
+}
+
+export interface IHost {
+    host: string,
+    port: number
+}
+
+export interface IDbConnection {
+    username: string,
+    password: string,
+    database: string,
+    hosts:    Array<IHost>
 }
