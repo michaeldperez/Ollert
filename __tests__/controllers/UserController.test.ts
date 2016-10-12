1010c21d383455ce479d741e40e1db0a8613e13f
Michael Perez <perez.michael.d@gmail.com>
2016-10-11 18:07:11 -0700

    Add UserController test scaffold

---
+++ __tests__/controllers/UserController.test.ts
@@ -0,0 +1,11 @@
+import User           from '../../models/User';
+import UserController from '../../controllers/UserController';
+import * as sinon     from 'sinon';
+
+describe('UserController Tests', () => {
+  describe('GET', () => {
+      it('should return a list of users', () => {
+
+      });
+  })
+});
