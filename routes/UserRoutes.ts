2fbbc839da7daf00b55649b14dd474aeff3d9f4f
Michael Perez <perez.michael.d@gmail.com>
2016-10-11 18:19:00 -0700

    Add jsDoc annotation to UserRouter

--- routes/UserRoutes.ts
+++ routes/UserRoutes.ts
@@ -3,6 +3,12 @@
 import * as express from 'express';
 import UserController from '../controllers/UserController';
 
+/**
+ * Router for User collection in database requests
+ * @function UserRouter 
+ * @params { UserController } UserCtrl - UserController instance
+ * @returns express Router
+ */
 export default function UserRouter(UserCtrl: UserController): express.Router {
     const Router: express.Router = express.Router();
 
