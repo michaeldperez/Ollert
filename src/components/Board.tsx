573b1a6d861def6e5ed4e06ab05253cceddb3ef0
Michael Perez <perez.michael.d@gmail.com>
2016-11-22 19:35:42 -0800

    Begin building out basic Board functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,59 @@
 import * as React from 'react';
+import { IList }  from '../../interfaces';
 
-export interface BoardProps { name: string; }
+interface BoardProps { name: string; }
+interface BoardState { lists: IList[] }
 
-export class Board extends React.Component<BoardProps, {}> {
+export class Board extends React.Component<BoardProps, BoardState> {
+    constructor(props: BoardProps) {
+        super(props);
+        this.state = {
+            lists: []
+        };
+    }
     render() {
-        return (<h1>{ this.props.name }</h1>);
+        return (
+            <div className="board">
+              <h1>{ this.props.name }</h1>
+              <div className='lists'></div>
+              <button onClick={ this._addList.bind(this) }> Add List </button>
+            </div>
+        );
+    }
+
+    componentWillMount() {
+        return this._getLists();
+    }
+
+    _getLists() {
+        const method: string       = 'GET';
+        const url: string          = '/users/${userId}/boards/${boardId}/lists';
+        const xhr: XMLHttpRequest  = new XMLHttpRequest();
+        
+        xhr.open(method, url, true);
+        xhr.onerror = () => { alert('There was an error retrieving your lists.' ); }
+        xhr.onload  = () => {
+            const lists = JSON.parse(xhr.responseText);
+            this.setState({ lists });
+        }
+        xhr.send();
+    }
+
+    _addList() {
+        const list: IList = { name: "", cards: [], createdAt: new Date(), updatedAt: null };
+
+        this.setState({
+            lists: this.state.lists.concat([ list ])
+        });
+        
+        /* Not DRY - extract to method */
+
+        const method: string       = 'POST';
+        const url: string          = '/users/${userId}/boards/${boardId}/lists';
+        const xhr: XMLHttpRequest  = new XMLHttpRequest();
+        
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(list);
     }
 }
